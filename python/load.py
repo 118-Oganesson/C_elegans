aea233d130eb2381ebcde57d54e454ce5bcc9a5d
118-Oganesson <p0eo18it@s.okayama-u.ac.jp>
2024-01-16 14:48:48 +0900

    Bearing_vs_Turing_biasのグラフをpythonでプロットできるようにした。

--- python/load.py
+++ python/load.py
@@ -12,3 +12,26 @@ def load_result_json(file_path):
     with open(file_path, "r") as json_file:
         data = json.load(json_file)
     return data
+
+
+def load_output_txt(file_path):
+    with open(file_path, "r") as txt_file:
+        # ファイルの各行を読み込んでリストに格納
+        lines = txt_file.read().split("\n")
+
+    # 各行での最大列数を求める
+    max_columns = max(len(line.split(", ")) for line in lines if line)
+
+    # 列ごとのデータを格納するためのリストを初期化
+    columns = [[] for _ in range(max_columns)]
+
+    # 行ごとにデータを列に振り分ける
+    for line in lines:
+        if line:
+            # 各行のデータをカンマで分割し、文字列からfloat64に変換
+            values = [float(value) for value in line.split(", ")]
+            # 各列にデータを振り分け
+            for i in range(len(values)):
+                columns[i].append(values[i])
+
+    return columns
